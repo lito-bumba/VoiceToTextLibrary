3a4947e3cb09c9b242c14ba7cd284ba76f60b0af
Lito Bumba <litocdbumba@gmail.com>
2024-06-03 04:21:54 -0300

    Sample using remote library

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -67,5 +67,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    implementation(project(path = ":library"))
+    implementation(libs.voicetotextlibrary)
 }
